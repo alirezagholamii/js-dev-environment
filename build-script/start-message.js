623263c767b4b4737eba64d58b18b2bbd9eddbe6
Alireza <alireza.gholami403@gmail.com>
2021-07-30 22:33:54 +0430

    feat: add babelrc

--- build-script/start-message.js
+++ build-script/start-message.js
@@ -1,5 +1,4 @@
-// import chalk from 'chalk';
+import chalk from 'chalk';
 
 /*eslint-disable no-console */
-console.log('run in dev mode');
-// console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...'));
