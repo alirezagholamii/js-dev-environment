fd2ceeb2b5811f9028e1deee3680a58366b36f54
Alireza <alireza.gholami403@gmail.com>
2021-07-30 20:27:09 +0430

    feat: add express

---
+++ build-script/src-server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const __dirname = path.resolve();
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
